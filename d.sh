bf8f1773434b9c15dae6483250fa639aee561c8f
mfoud444 <mfoud444@gmail.com>
2024-07-08 22:27:03 +0300

    first commit

--- d.sh
+++ d.sh
@@ -3,6 +3,6 @@
 # git init
 git add .
 git commit -m "first commit"
-git branch -M main
+# git branch -M main
 # git remote add origin https://github.com/mfoud444/mousarate.git
 git push -u origin main
